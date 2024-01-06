4d517ca9a7d682a24526c1995ff4ecff91c13c0c
simon020286 <jemisi@hotmail.it>
2024-01-06 09:13:11 +0100

    Add sitemap
    Co-authored-by: codewithheart <codewithheart.dev>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyle: false,
-    })
-  ],
+  integrations: [tailwind({
+    applyBaseStyle: false
+  }), sitemap()],
   scopedStyleStrategy: "where",
   redirects: {
     "/": "/posts"
